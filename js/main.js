54fed3f0310827b575ab267fcb10d59badf416f0
Córdova Carlos <ccordova.zosma@gmail.com>
2020-08-29 13:38:06 -0500

    Versión corregida

--- js/main.js
+++ js/main.js
@@ -6,6 +6,23 @@ function openMenu() {
     $('.js-menu-container1').removeClass('ocultar');
     $('.js-menu-container2').addClass('ocultar');
   }
+  function openMenu2() {
+    $('.tiptext').addClass('mostrar');
+    $('.menu-button3').removeClass('ocultar');  
+    $('.menu-button2').addClass('ocultar'); 
+    $('.icono2').addClass('oscuro');  
+    $('.cont__icono').addClass('oscuro');  
+
+  }
+  function closeMenu2() {
+    $('.tiptext').removeClass('mostrar');
+    $('.menu-button3').addClass('ocultar');  
+    $('.menu-button2').removeClass('ocultar'); 
+    $('.icono2').removeClass('oscuro');  
+    $('.cont__icono').removeClass('oscuro'); 
+ 
+  }
+
 
   // Document Ready
 jQuery(document).ready(function($){ 
@@ -16,6 +33,17 @@ jQuery(document).ready(function($){
   
     $('.js-menu-close').click(function(){
       closeMenu();
+    });
+
+    $('.menu-button2').click(function(){
+         
+      openMenu2(); 
+    });
+    
+    $('.menu-button3').click(function(){
+      
+      closeMenu2();      
+
     });
   
   });
